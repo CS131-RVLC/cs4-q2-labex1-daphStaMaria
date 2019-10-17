fa4736bcb5b80269d8440a9470a200714503e445
GLUON <GLUON@172.25.16.148>
2019-10-17 15:01:38 +0800

    lol commit

--- ArrayListsQ2LabEx1/src/arraylistsq2labex1/ArrayDemo1.java
+++ ArrayListsQ2LabEx1/src/arraylistsq2labex1/ArrayDemo1.java
@@ -4,22 +4,33 @@ import java.util.*;
 class Student {
     String Name;
     int Score;
-    
-    
+   
+   
     public void displayArray(ArrayList<Student> AS) {
 //<your code to display the array elements>
-    System.out.println("abc");
+        for(int i=0; i<AS.size(); i++){
+            System.out.print(AS.get(i).Name +" ");
+            System.out.print("\n");
+        }
     }
-    
+   
      public void searchHighestScore(ArrayList<Student> AS) {
 //<your code to search for the highest score in the array>
-
+        int highScore;
+        highScore = AS.get(0).Score;
+        for(int i=0; i<AS.size();i++){
+            if(AS.get(i).Score>highScore){
+                highScore = AS.get(i).Score;
+            }
+        }
+        System.out.println("hs: " + highScore);
+       
     }    
     public void sortScores(ArrayList<Student> AS) {
-//        	<your code to sort the array>
+//         <your code to sort the array>
 
     }
-    
+   
     public void sortNames(ArrayList<Student> AS) {
 //<your code to sort the array according the names>      
    }
@@ -29,14 +40,14 @@ public class ArrayDemo1 {
     public static void main(String[] args) {
         String[] NameI = {"Bok", "Mik", "Jak", "Luds", "Kim"};
         int[] ScoreI = {90, 96, 91, 97, 99};
-        
+       
         ArrayList<Student> StudentArrayList = new ArrayList<Student>();
         for (int i = 0; i <= 4; i++) {
             StudentArrayList.add(new Student());
             StudentArrayList.get(i).Name = NameI[i];
             StudentArrayList.get(i).Score = ScoreI[i];
         }
-        
+       
        
         Student method = new Student();
 
@@ -45,14 +56,13 @@ public class ArrayDemo1 {
         method.searchHighestScore(StudentArrayList);
 
         method.sortScores(StudentArrayList);
-        
+       
         method.displayArray(StudentArrayList);
 
         method.sortNames(StudentArrayList);
-        
+       
         method.displayArray(StudentArrayList);        
-        
+       
    
     }
 }
-
